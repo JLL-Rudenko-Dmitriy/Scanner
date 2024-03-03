c0d31dfe074473e4b25aceec250898bebb6e75bb
JLL-Rudenko-Dmitriy <jll.rudenkodmitriy@gmail.com>
2024-03-03 16:24:11 +0300

    result code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <Scanner.cpp>
+#include <parser.cpp>
 
-int main(int argc, char **argv) {
-    std::cout << argc << ' ' << argv[0];
+int main(int argc, const char **argv) {
+    
+    ArgParser* parser = new ArgParser(argc, argv);
+    if (parser->isCorrect()) {
+        std::string ip = parser->getIp();
+
+        if (parser->isRanged()) {
+            std::pair<uint32_t,uint32_t> range = parser->getRange();
+            for (uint32_t port = range.second; port < range.first; ++port) {
+                tryConnect(ip,port);
+            }
+        } else {
+            for (auto port: parser->getValues()) {
+                tryConnect(ip,port);
+            }
+        } 
+    }
     return 0;
 }
